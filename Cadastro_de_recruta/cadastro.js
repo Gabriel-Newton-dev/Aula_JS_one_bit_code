debf3290a0512db42a4ebbcc013bcb1a4196abd2
Gabriel <gabriel@MacBook.local>
2022-09-16 00:23:56 -0700

    inclusao do input na página WEB

--- Cadastro_de_recruta/cadastro.js
+++ Cadastro_de_recruta/cadastro.js
@@ -1,9 +1,14 @@
-let primeiroNome = "Gabriel" 
-let sobrenome = "Newton."
+let primeiroNome = "Nome" 
+let sobrenome = "Sobrenome"
 let campoDeEstudo = "Tecnologia da Informação."
-let anoDeNascimento = 1986
+let anoDeNascimento
+
+primeiroNome = window.prompt("Digite o seu primento nome: ")
+sobrenome = window.prompt("Digite o seu sobrenome:")
+campoDeEstudo = window.prompt("Digite seu campo de estudo:")
+anoDeNascimento = window.prompt("Digite o ano do seu nascimento")
 
 let idade = 2022 - anoDeNascimento
 
-console.log(primeiroNome, sobrenome,campoDeEstudo, idade)
+console.log("Nome:",primeiroNome, sobrenome,"- Campo de estudo:", campoDeEstudo,"-", idade, "anos de idade")
 
