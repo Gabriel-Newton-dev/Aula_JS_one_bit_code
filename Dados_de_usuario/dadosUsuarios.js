7c184c87bdad03386b1738aff6e6509c74cef9f7
Gabriel <gabriel@MacBook.local>
2022-09-21 18:57:57 -0700

    Exercicio pegando dados dos usuarios com JS

---
+++ Dados_de_usuario/dadosUsuarios.js
@@ -0,0 +1,11 @@
+alert("Bem-vindo! A seguir pediremos que informa alguns dados.")
+
+const nome = prompt("Digite seu nome completo por favor:")
+const idade = prompt("Digite a sua idade por favor:")
+const confIdade = prompt("Confirme a sua idade: ")
+
+alert(
+    "Nome completo: "+nome+
+    "\nIdade:"+idade+
+    "\nConfirmação da idade:"+confIdade
+)
