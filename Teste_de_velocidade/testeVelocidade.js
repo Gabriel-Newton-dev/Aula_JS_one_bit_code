1175b4f00b813beed73e3506cce60d27ced03e77
Gabriel <gabriel@MacBook.local>
2022-09-22 17:04:17 -0700

    Implementacao do exercicio teste de velocidade

--- Teste_de_velocidade/testeVelocidade.js
+++ Teste_de_velocidade/testeVelocidade.js
@@ -6,9 +6,9 @@ const carro2 = prompt("Digite o nome do carro número 2: ")
 const velocidade2 = prompt("Qual a velocidade máxima desse carro ? ")
 
 if(velocidade1 > velocidade2 ){
-    alert("Carro 1 é mais rápido que o carro2")
+    alert("O " + carro1 + " é mais rápido que o "+carro2 +" e a sua velocidade máxima é "+velocidade1+" KM/h")
 }else if(velocidade1 < velocidade2){
-    alert("O Carro 2 é mais rápido que o carro2")
+    alert("O " + carro2 + " é mais rápido que o "+carro1 +" e a sua velocidade máxima é "+velocidade2+ " KM/h")
 }else if(velocidade1 === velocidade2){
-    alert("Ambos carros correm a mesma velocidade -- empataram. ")
+    alert("Ambos carros correm a mesma velocidade(empate).")
 }
