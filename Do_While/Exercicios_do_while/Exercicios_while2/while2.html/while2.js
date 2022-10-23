dc4de12c6f4e2e0b5a9a33215b670c317a0254f0
Gabriel <gabriel@MacBook.local>
2022-10-23 00:49:47 -0700

    do while - exercicios 2

---
+++ Do_While/Exercicios_do_while/Exercicios_while2/while2.html/while2.js
@@ -0,0 +1,28 @@
+let escolha
+
+do {
+    escolha = prompt("Qual carro você deseja comprar " + "\n1. Cruze" + "\n2. New Civic" + "\n3. Audi" + "\n4. Eclipse" + "\n5. Nenhuma das opções anteriores" )
+switch (escolha){
+    case "1": 
+        alert("Vocé escolheu a opção 1 - Cruze Turbo")
+        break
+    case "2": 
+        alert("Vocé escolheu a opção 2 - New Civic")
+        break
+    case "3": 
+        alert("Vocé escolheu a opção 3 - Audi TT")
+        break
+    case "4": 
+    alert("Vocé escolheu a opção 4 - Mitsubish Eclipse")
+        break
+        case "5": 
+        break
+        default:
+        alert("Opção inválida !!")
+}
+}
+while(escolha !== "5"){
+    alert("Desculpe não temos outros veículos disponiveís !")
+}
+
+
